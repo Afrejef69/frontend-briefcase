52d78adf89a764a4992654f452f40b7dad16d740
Afrejef69 <pxndxjeffrey.jr@gmail.com>
2024-05-29 12:47:48 -0600

    Change query from client to server in blog list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,6 @@
-'use client';
 import { BlogCard, Loading } from "@/components";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { parsedEnv } from "../../../env";
 
 interface Blog {
     id: number;
@@ -10,23 +9,15 @@ interface Blog {
     genre: string;
 }
 
-export default function Blog() {
-    const [blog, setBlog] = useState<Blog[]>([]);
-
-    useEffect(() => {
-        const fetchBlog = async () => {
-            const response = await axios.get<Blog[]>(process.env.NEXT_PUBLIC_API_URL + '/blog' ?? "");
-            setBlog(response.data);
-        };
-        fetchBlog();
-    }, []);
+export default async function Blog() {
+    const response = await axios.get<Blog[]>(parsedEnv.API_URL + '/blog')
 
     return (
         <main className="flex flex-col items-center p-24 min-h-screen animate-fadeIn">
             <h1 className="text-3xl font-bold mb-4 text-center">Lista de Blogs</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {
-                        blog.map((blog) => (
+                        response.data.map((blog) => (
                             <BlogCard 
                                 key={blog.id}
                                 id={blog.id}
